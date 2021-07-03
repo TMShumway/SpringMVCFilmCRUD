03aafb09d31fef4280e06dd781265b93b03db57f
TMShumway <ryujindra@gmail.com>
2021-07-03 11:14:40 -0600

    sync with partner

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,7 +1,10 @@
 package com.skilldistillery.film.controllers;
 
 
+import com.skilldistillery.film.database.DatabaseAccessorObject;
 import com.skilldistillery.film.entities.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,18 +12,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class FilmController {
 	
-//	@Autowired
-//	FilmDAO filmDAO = new FilmDAO();
+	@Autowired
+	DatabaseAccessorObject filmDAO;
 	
 	@RequestMapping(path = {"/","home.do"})
 	public String index() {
 		return "WEB-INF/home.jsp";
 	}
 	
-	@RequestMapping(path = "filmDetails.do", param = "filmId")
+	@RequestMapping(path = "filmDetails.do", params = "filmId")
 	public ModelAndView getFilmDetails(int filmId) {
 		ModelAndView mv = new ModelAndView();
-		Film film = filmDAO.getFilmById(filmId);
+		Film film = filmDAO.findFilmById(filmId);
 		mv.addObject(film);
 		mv.setViewName("WEB-INF/filmResults.jsp");
 		return mv;
