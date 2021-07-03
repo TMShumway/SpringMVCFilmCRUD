c996a1fe3c6a46add60ce6b1e8ac85202cdc0117
TMShumway <ryujindra@gmail.com>
2021-07-03 10:03:30 -0600

    addtion to controller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,17 +1,29 @@
 package com.skilldistillery.film.controllers;
 
+
+import com.skilldistillery.film.entities.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FilmController {
 	
-	@Autowired
-	FilmDAO filmDAO;
+//	@Autowired
+//	FilmDAO filmDAO = new FilmDAO();
 	
 	@RequestMapping(path = {"/","home.do"})
 	public String index() {
 		return "WEB-INF/home.jsp";
+	}
+	
+	@RequestMapping(path = "filmDetails.do", param = "filmId")
+	public ModelAndView getFilmDetails(int filmId) {
+		ModelAndView mv = new ModelAndView();
+		Film film = filmDAO.getFilmById(filmId);
+		mv.addObject(film);
+		mv.setViewName("WEB-INF/home.jsp");
+		return mv;
 	}
 	
 }
